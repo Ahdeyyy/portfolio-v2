a15eb9060b31a3834796775624e48230463d51f8
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2024-02-22 00:47:39 +0100

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script>
+	import '../app.pcss';
+	import { GithubLogo, TwitterLogo } from 'svelte-radix';
+	import { Button } from '$lib/components/ui/button';
+	import '@fontsource-variable/inter';
+</script>
+
+<main class="container relative mx-auto space-y-2 p-5">
+	<nav class="sticky left-0 top-0 w-full p-4 backdrop-blur-md">
+		<ul class="flex content-center justify-between">
+			<li class="size-8 rounded-full bg-primary"></li>
+			<li class="space-x-5">
+				<Button
+					variant="outline"
+					class="rounded-full"
+					size="icon"
+					href="https://github.com/Ahdeyyy"
+				>
+					<GithubLogo class="size-4" />
+				</Button>
+
+				<Button variant="outline" class="rounded-full" size="icon" href="https://x.com/Ahdeyy21">
+					<TwitterLogo class="size-4" />
+				</Button>
+			</li>
+		</ul>
+	</nav>
+	<slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<section
+	class="grid h-[100vh] place-content-center space-y-1 bg-gradient-to-br from-primary/5 to-primary/0"
+>
+	<h1
+		class="bg-gradient-to-r from-teal-400 via-amber-400 to-red-400 bg-clip-text text-center font-sans text-5xl font-bold uppercase text-transparent md:text-6xl"
+	>
+		Adeala Adebusuyi
+	</h1>
+	<h2 class="text-center font-sans text-lg font-semibold text-muted">Software developer</h2>
+	<p class="text-pretty break-words text-center font-sans text-primary md:text-balance">
+		Passionate software developer with a background in computer science. specialties include
+		frontend development with html, css, javascript, react , backend development with golang, nodejs
+	</p>
+</section>
