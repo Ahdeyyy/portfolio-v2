bf6ebcffbcb64992a519ff5c144b34be24541f89
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2024-02-23 00:14:57 +0100

    feat: added projects

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,12 +4,18 @@
 	import { Button } from '$lib/components/ui/button';
 	import '@fontsource-variable/inter';
 	import '@fontsource/geist-sans';
+	import * as Avatar from '$lib/components/ui/avatar';
 </script>
 
-<main class="container relative mx-auto space-y-2 p-2 md:p-5">
+<main class="container relative mx-auto p-2 md:p-5">
 	<nav class="fixed left-0 top-0 z-50 w-full p-2 backdrop-blur-lg md:p-4">
 		<ul class="flex content-center justify-between">
-			<li class="size-8 rounded-full bg-primary"></li>
+			<li class="rounded-full">
+				<Avatar.Root>
+					<Avatar.Image src="/adebusuyi.jpg" class="object-cover" alt="@adebusuyi" />
+					<Avatar.Fallback>AA</Avatar.Fallback>
+				</Avatar.Root>
+			</li>
 			<li class=" space-x-2 md:space-x-5">
 				<Button
 					variant="outline"
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
+<script lang="ts">
+	import { AspectRatio } from '$lib/components/ui/aspect-ratio';
+	import { Badge } from '$lib/components/ui/badge';
+	import { Button } from '$lib/components/ui/button';
+</script>
+
 <section
-	class="full-width grid h-[100vh] place-content-center space-y-1 overflow-x-hidden bg-slate-900"
+	class="full-width grid h-screen place-content-center space-y-2 overflow-x-hidden bg-slate-900"
 >
 	<h1
 		class="bg-gradient-to-r from-teal-400 via-amber-400 to-red-400 bg-clip-text text-center font-sans text-5xl font-bold uppercase text-transparent md:text-6xl"
@@ -11,6 +17,32 @@
 		Passionate software developer with a background in computer science. specialties include
 		frontend development with html, css, javascript, react , backend development with golang, nodejs
 	</p>
+</section>
+
+<section class="h-screen space-y-2 p-5">
+	<ul class="h-full">
+		<li class="h-1/5">
+			<article class="grid gap-12 p-3 md:flex md:p-5">
+				<AspectRatio ratio={16 / 9} class="bg-muted">
+					<img
+						class="h-full w-full rounded-lg object-cover md:object-contain"
+						src="/reader.png"
+						alt="epub reader"
+					/>
+				</AspectRatio>
+				<div class="grid place-content-center gap-4">
+					<p>Svelte epub reader</p>
+					<p>
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto eum ullam sequi
+						debitis unde aliquid deserunt aliquam amet in numquam.
+					</p>
+					<Button>Lorem ipsum dolor sit amet.</Button>
+					<Button>Lorem ipsum dolor sit amet.</Button>
+					<Badge>Go</Badge>
+				</div>
+			</article>
+		</li>
+	</ul>
 </section>
 
 <style>
