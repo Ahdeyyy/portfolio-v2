b2aed48920405a71fef3bf67d9b7a8871097cbff
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2024-02-25 20:57:05 +0100

    feat: project component mock

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 	class="full-width grid h-screen place-content-center space-y-2 overflow-x-hidden bg-slate-900"
 >
 	<h1
-		class="bg-gradient-to-r from-teal-400 via-amber-400 to-red-400 bg-clip-text text-center font-sans text-5xl font-bold uppercase text-transparent md:text-6xl"
+		class="bg-gradient-to-r from-amber-400 to-red-400 bg-clip-text text-center font-sans text-5xl font-bold uppercase text-transparent md:text-6xl"
 	>
 		Adeala Adebusuyi
 	</h1>
@@ -20,25 +20,34 @@
 </section>
 
 <section class="h-screen space-y-2 p-5">
+	<h3
+		class="bg-gradient-to-r from-red-500 to-amber-400 bg-clip-text text-center font-sans text-2xl font-semibold uppercase text-transparent"
+	>
+		Projects
+	</h3>
 	<ul class="h-full">
 		<li class="h-1/5">
-			<article class="grid gap-12 p-3 md:flex md:p-5">
-				<AspectRatio ratio={16 / 9} class="bg-muted">
-					<img
-						class="h-full w-full rounded-lg object-cover md:object-contain"
-						src="/reader.png"
-						alt="epub reader"
-					/>
-				</AspectRatio>
-				<div class="grid place-content-center gap-4">
+			<article class="grid gap-12 p-3 md:grid-flow-col md:grid-cols-5 md:p-5">
+				<img
+					class="h-full w-full rounded-lg object-cover md:col-span-3"
+					src="/reader.png"
+					alt="epub reader"
+				/>
+				<div
+					class="grid place-content-center gap-4 space-y-2 text-balance text-center md:col-span-2"
+				>
 					<p>Svelte epub reader</p>
 					<p>
 						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto eum ullam sequi
 						debitis unde aliquid deserunt aliquam amet in numquam.
 					</p>
-					<Button>Lorem ipsum dolor sit amet.</Button>
-					<Button>Lorem ipsum dolor sit amet.</Button>
-					<Badge>Go</Badge>
+					<div class="flex justify-center gap-4">
+						<Button>Repository</Button>
+						<Button>Release</Button>
+					</div>
+					<div class="flex">
+						<Badge>Go</Badge>
+					</div>
 				</div>
 			</article>
 		</li>
